9db753c257123832c2e75f1c740461ea7e125e30
Sujan <najussujan@gmail.com>
2015-12-13 22:28:00 -0600

    JSON and XML configuration along with ajax

--- src/main/java/edu/mum/controller/MinutesController.java
+++ src/main/java/edu/mum/controller/MinutesController.java
@@ -1,9 +1,15 @@
 package edu.mum.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import edu.mum.model.Activity;
 import edu.mum.model.Exercise;
 
 @Controller
@@ -12,7 +18,27 @@ public class MinutesController {
 	@RequestMapping(value = "/addMinutes")
 	public String addMinutes(@ModelAttribute("exercise") Exercise exercise) {
 		System.out.println("exercise " + exercise.getMinutes());
+		System.out.println("exercise activity" + exercise.getActivity());
 		return "addMinutes";
+	}
+
+	@RequestMapping(value = "/activities", method = RequestMethod.GET)
+	public @ResponseBody List<Activity> findAllActivities() {
+		List<Activity> activities = new ArrayList<>();
+
+		Activity run = new Activity();
+		run.setDesc("Run");
+		activities.add(run);
+
+		Activity bike = new Activity();
+		bike.setDesc("Bike");
+		activities.add(bike);
+
+		Activity swim = new Activity();
+		swim.setDesc("Swim");
+		activities.add(swim);
+
+		return activities;
 	}
 
 	// @RequestMapping(value = "/addMoreMinutes")
